cd7abba6132321fd6c631aba653ffa2d8a27b8f9
fabrygameplay-rgb <tuemail@users.noreply.github.com>
2025-10-05 20:32:50 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const NúmeroDiscreto = Math.floor(Math.random() * 10) + 1;
+const Entra = document.getElementById("Número-Del-Usuario");
+const Botón = document.querySelector(".botón");
+const Sale = document.querySelector(".contenedor-imagen"); // ✅ usamos class
+
+Botón.addEventListener("click", () => {
+    const Intento = parseInt(Entra.value);
+
+    if (isNaN(Intento)) {
+        Sale.style.backgroundColor = "cornsilk";
+        Sale.innerText = "❌, ¿UCV, no?";
+    } else if (Intento === NúmeroDiscreto) {
+        Sale.style.backgroundColor = "seagreen";
+        Sale.innerText = "🎉, Ding-Dong";
+    } else if (Intento < NúmeroDiscreto) {
+        Sale.style.backgroundColor = "salmon";
+        Sale.innerText = "⬆️, Tont@";
+    } else {
+        Sale.style.backgroundColor = "salmon";
+        Sale.innerText = "⬇️, Tont@";
+    }
+
+    Entra.value = "";
+    Entra.focus();
+});
